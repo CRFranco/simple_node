e9eda9aba4ff8307b2d9acad4797a7baf8baddbe
franco.cr@gmail.com <franco.cr@gmail.com>
2021-08-12 14:56:37 -0300

    environment variables

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
+  const favoriteFood = process.env.AWS_MEDIA_BUCKET;
+  
   while(true) {
-    console.log('Containers rule!');
+    console.log(`My favorite food is ${favoriteFood}`);
     await sleep(5000);
   }
 }
